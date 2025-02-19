2a5ff388fc147a366f74fedc96846cbd000571fe
chuksoo <mexx4me@yahoo.com>
2025-02-19 09:27:09 -0900

    Started Unit 1

---
+++ TIP102 - Intermediate Technical Interview Prep/Unit 1 practice.py
@@ -0,0 +1,16 @@
+# Session 1: Strings & Arrays
+# Standard Problem Set Version 1
+''' Problem 1: Hundred Acre Wood
+Write a function welcome() that prints the string "Welcome to The Hundred Acre Wood!".'''
+def welcome():
+    print("Welcome to The Hundred Acre Wood!")
+
+welcome()
+print()
+
+'''Problem 2: Greeting
+Write a function greeting() that accepts a single parameter, a string name, and prints the string 
+"Welcome to The Hundred Acre Wood <name>! My name is Christopher Robin."
+'''
+def greeting(name):
+	pass
