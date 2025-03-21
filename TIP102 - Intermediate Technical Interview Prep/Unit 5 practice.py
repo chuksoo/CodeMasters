f5dd3fa3b960128985c2a22eadc122c45739ac13
Chuksoo <mexx4me@yahoo.com>
2025-03-21 10:22:36 -0800

    Working on Unit 5

--- TIP102 - Intermediate Technical Interview Prep/Unit 5 practice.py
+++ TIP102 - Intermediate Technical Interview Prep/Unit 5 practice.py
@@ -242,3 +242,31 @@ node_two.next = node_three
 
 # Input List: 5 -> 6 -> 7
 print_linked_list(halve_list(node_one))
+
+'''Problem 5: Remove Last
+Write a function delete_tail() that accepts the head of a linked list and removes the last node in the list. Return the head of the modified list.
+
+Note: The "tail" of a list is the last element in the linked list. It is equivalent to lst[-1] in a normal list.'''
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+# For testing
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.value, end=" -> " if current.next else "\n")
+        current = current.next
+
+def delete_tail(head):
+    pass
+
+butterfly = Node("Common Butterfly")
+ladybug = Node("Ladybug")
+beetle = Node("Scarab Beetle")
+butterfly.next = ladybug
+ladybug.next = beetle
+
+# Input List: butterfly -> ladybug -> beetle
+print_linked_list(delete_tail(butterfly))
