1cfb1a5e7e96e993ff6ecd7592b8ded4ac722a14
chuksoo <mexx4me@yahoo.com>
2025-03-25 09:55:36 -0800

    Updated Unit 5 - Standard Problem Set 2 - Convert Single to Doubly linked list

--- TIP102 - Intermediate Technical Interview Prep/Unit 5 practice.py
+++ TIP102 - Intermediate Technical Interview Prep/Unit 5 practice.py
@@ -1,5 +1,8 @@
+###################################################
 # Session 1: OOP & Linked Lists
+###################################################
 # Standard Problem Set Version 1
+# -------------------------------------------------
 '''Problem 1: New Horizons
 Step 1: Copy the following code into your IDE.
 
@@ -294,7 +297,9 @@ def print_linked_list(head):
         current = current.next
     return 
 
+# -------------------------------------------------
 # Standard Problem Set Version 2
+# -------------------------------------------------
 '''Problem 1: Player Class
 Step 1: Copy the following code into your IDE.
 
@@ -510,7 +515,9 @@ def chase_list(head):
         current = current.next
     return 
 
+# -------------------------------------------------
 # Advanced Problem Set Version 1
+# -------------------------------------------------
 '''Problem 6: Got One!
 Imagine that behind the scenes, Animal Crossing uses a linked list to represent the order fish will appear to a player who is fishing in the river. The head of the list represents the next fish that a player will catch if they keep fishing.
 
@@ -592,9 +599,11 @@ def restock(head, new_fish):
     current.next = new_node
     return head
 
-
+###################################################
 # Session 2: Linked Lists
+###################################################
 # Standard Problem Set Version 1
+# -------------------------------------------------
 '''Problem 1: Mutual Friends
 In the Villager class below, each villager has a friends attribute, which is a list of other villagers they are friends with.
 
@@ -845,6 +854,226 @@ def print_reverse(tail):
     while current is not None:
         print(current.value, end="  " if current.prev else "\n")
         current = current.prev
+
+# -------------------------------------------------
+# Standard Problem Set Version 2
+# -------------------------------------------------
+'''Problem 1: Calculate Tournament Placement
+In the Player class below, each player has a race_outcomes attribute which holds a list of integers describing what place they came in for each race in a tournament.
+
+Write a method get_tournament_place() that takes in one parameter, opponents, a list of other player objects also participating in the tournament, and returns the place in the overall tournament.
+
+Rank in the tournament is determined by the lowest average race outcome. (1st place is better than 2nd!)
+Each opponent in opponents is guaranteed to have participated in the same number of races as the current player.'''
+class Player_chess:
+    def __init__(self, character, kart, outcomes):
+        self.character = character
+        self.kart = kart
+        self.items = []
+        self.race_outcomes = outcomes
+
+    def get_tournament_place(self, opponents):
+        my_average = sum(self.race_outcomes) / len(self.race_outcomes)
+
+        better_count = 0
+        for opponent in opponents:
+            opp_sum = 0
+            for opp_outcome in opponent.race_outcomes:
+                opp_sum += opp_outcome
+            opp_average = opp_sum / len(opponent.race_outcomes)
+
+            if opp_average < my_average:
+                better_count += 1
+        
+        return better_count + 1
+
+'''Problem 2: Update Linked List Sequence
+A linked list is a data structure that allows us to store pieces of data sequentially, similar to a normal list or array. 
+The key difference between a linked list and a normal list is how each element is stored in a computer’s memory.
+
+In a normal list, individual elements are stored in adjacent memory locations according to their order in the list. If we know where the first element is stored, it's easy to access any other element in the list.
+
+In a linked list, individual elements, called nodes, are not stored in sequential memory locations. Each node may be stored in an unrelated memory location. 
+To connect nodes into a sequential list, each node stores a reference or pointer to the next node in the list.
+
+Using the provided Node class and the linked list below, update the current linked list shy_guy -> diddy_kong -> dry_bones to shy_guy -> link -> diddy_kong -> toad -> dry_bones.
+
+A function print_linked_list() that accepts the head, or first element, of a linked list and prints the values of the list has also been provided for testing purposes.'''
+class Node_v2:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+# For testing
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.value, end=" -> " if current.next else "\n")
+        current = current.next
+
+shy_guy = Node_v2("Shy Guy")
+diddy_kong = Node_v2("Diddy Kong")
+dry_bones = Node_v2("Dry Bones")
+
+# Add code to update the list here
+link_node = Node_v2("Link")
+toad = Node_v2("Toad")
+shy_guy.next = link_node  
+link_node.next = diddy_kong
+diddy_kong.next = toad
+toad.next = dry_bones
+
+'''Problem 3: Insert Node as Second Element
+Write a function add_second() that takes in the head of a linked list and a value val as parameters. It should insert val as the second node in the linked list and return the head of the linked list. 
+(You can assume head is not None.)
+
+Note: The "head" of a linked list is the first element in the linked list. It is equivalent to lst[0] of a normal list.'''
+def add_second(head, val):
+    new_node = Node_v2(val)
+
+    new_node.next = head.next
+    head.next = new_node
+    return head
+
+'''Problem 4: Increment Linked List Node Values
+Write a function increment_ll() that takes in the head of a linked list of integer values and returns the same list, but with each node's value incremented by 1. Return the head of the list.
+'''
+def increment_ll(head):
+    if head is None:
+        return head
+    
+    current = head
+    while current:
+        current.value = current.value + 1
+        current = current.next
+    return head
+
+'''Problem 5: Copy Linked List
+Write a function copy_ll() that takes in the head of a linked list and creates a complete copy of that linked list.
+
+The function should return the head of a new linked list which is identical to the given list in terms of its structure and contents, but does not use any of the node objects from the original list.'''
+def copy_ll(head):
+    # if the original list is empty, the copy will be empty
+    if head is None:
+        return None
+
+    # create new head node
+    new_head = Node_v2(head.value)
+    
+    orig_ptr = head.next
+    copy_ptr = new_head
+
+    while orig_ptr is not None:
+        new_node = Node_v2(orig_ptr.value)
+        copy_ptr.next = new_node
+
+        # advance both pointers
+        copy_ptr = copy_ptr.next
+        orig_ptr = orig_ptr.next
+    return new_head
+
+'''Problem 6: Making the Cut
+Imagine that a linked list is used to track the order players finished in a race. Write a function top_n_finishers() that takes in the head of a linked list and a non-negative integer n as parameters.
+
+The function should return a list of the values of the first n nodes.
+
+If n is greater than the length of the linked list, return a list of the values of all nodes in the linked list.'''
+
+def top_n_finishers(head, n):
+    top_lst = []
+    current = head
+    while current is not None:
+        top_lst.append(current.value)
+        current = current.next
+
+    if n < len(top_lst):
+        return top_lst[:n]
+    return top_lst
+    
+'''Problem 7: Remove Racer
+Write a function remove_racer() that takes in the head of a linked list and a value racer as parameters.
+
+The function should remove the first node with the value racer from the linked list and return the head of the modified list. If racer is not in the list, return the head of the original list.'''
+def remove_racer(head, racer):
+    if head is None:
+        return None
+    
+    if head.value == racer:
+        new_head = head.next
+        head.next = None
+        return new_head
+    
+    current = head
+    previous = None
+    while current is not None:
+        if current.value == racer:
+            previous.next = current.next 
+            current.next = None
+            break
+        previous = current
+        current = current.next
+    return head
+
+'''Problem 8: Array to Linked List
+Write a function arr_to_ll() that accepts an array of Player instances arr and converts arr into a linked list. The function should return the head of the linked list. If arr is empty, return None.
+
+A function print_linked_list() which accepts the head, or first element, of a linked list and prints the character attribute of each Player in the linked list has also been provided for testing purposes.'''
+class Player_array_to_linkedlist:
+    def __init__(self, character, kart):
+        self.character = character
+        self.kart = kart
+        self.items = []
+
+class Node_array_to_linkedlist:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+# For testing
+def print_linked_list_char(head):
+    current = head
+    while current:
+        print(current.value.character, end=" -> " if current.next else "\n")
+        current = current.next
+
+def arr_to_ll(arr):
+    if not arr:
+        return None
+    
+    current_ptr = 0
+    head = Node_array_to_linkedlist(arr[current_ptr])
+    current = head
+    for current_ptr in range(1, len(arr)):
+        new_node = Node_array_to_linkedlist(arr[current_ptr])
+        current.next = new_node
+        current = current.next
+    return head
+
+'''Problem 9: Convert Singly Linked List to Doubly Linked List
+One of the drawbacks of a linked list is that it's difficult to go backwards, because each Node only knows about the Node in front of it. (E.g., A -> B -> C)
+
+A doubly linked list solves this problem! Instead of just having a next attribute, a doubly linked list also has a prev attribute that points to the Node before it. (E.g., A <-> B <-> C)
+
+Update the code below to convert the singly linked list to a doubly linked list.
+
+Two functions, print_linked_list() and print_linked_list_backwards(), have been provided for testing purposes. print_linked_list() accepts the head of a linked list and 
+prints the values of each node in the list, starting at the head and iterating in a forward direction. print_linked_list_backwards() accepts the tail of a linked list and 
+prints the values of each node in the list, starting at the tail and iterating in a backward direction.'''
+class Node_doubly:
+    def __init__(self, value, next=None, prev=None):
+        self.value = value
+        self.next = next
+        self.prev = prev
+
+def print_linked_list_backwards(tail):
+    pass
+
+
+
+
+
+    
+
 
 
 
@@ -1155,7 +1384,90 @@ if __name__ == "__main__":
     kk_slider.next = saharah
     saharah.prev = kk_slider
     kk_slider.prev = isabelle
-
     # Linked List: Isabelle <-> K.K. Slider <-> Saharah
     print_reverse(saharah)
+    print()
+    print("------ # Standard Problem Set Version 2 ------ ")
+    print("Problem 1: Calculate Tournament Placement")
+    player1 = Player_chess("Mario", "Standard", [1, 2, 1, 1, 3])
+    player2 = Player_chess("Luigi", "Standard", [2, 1, 3, 2, 2])
+    player3 = Player_chess("Peach", "Standard", [3, 3, 2, 3, 1])
+    opponents = [player2, player3]
+    print(player1.get_tournament_place(opponents))
+    print()
+    print("Problem 2: Update Linked List Sequence")
+    print("Current List:")
+    print_linked_list(shy_guy)
+    print()
+    print("Problem 3: Insert Node as Second Element")
+    original_list_head = Node_v2("banana")
+    second = Node_v2("blue shell")
+    third = Node_v2("bullet bill")
+    original_list_head.next = second
+    second.next = third
+    print("Original linked list")
+    print_linked_list(original_list_head)
+    # Linked list: "banana" -> "blue shell" -> "bullet bill"
+    new_list = add_second(original_list_head, "red shell")
+    print("Linked list after insertion")
+    print_linked_list(new_list)
+    print()
+    print("Problem 4: Increment Linked List Node Values")
+    node_one = Node(5)
+    node_two = Node(6)
+    node_three = Node(7)
+    node_one.next = node_two
+    node_two.next = node_three
+    # Input List: 5 -> 6 -> 7
+    print("Original linked list")
+    print_linked_list(node_one)
+    print("Linked list after increment")
+    print_linked_list(increment_ll(node_one))
+    print()
+    print("Problem 5: Copy Linked List")
+    mario = Node_v2("Mario")
+    daisy = Node_v2("Daisy")
+    luigi = Node_v2("Luigi")
+    mario.next = daisy
+    daisy.next = luigi
+
+    # Linked List: Mario -> Daisy -> Luigi
+    copy = copy_ll(mario)
+    # Change original list -- should not affect the copy
+    mario.value = "Original Mario"
+    print_linked_list(mario)
+    print_linked_list(copy) 
+    print()
+    print("Problem 6: Making the Cut")
+    head = Node_v2("Daisy", Node_v2("Mario", Node_v2("Toad", Node_v2("Yoshi"))))
+    # Linked List: Daisy -> Mario -> Toad -> Yoshi
+    print(top_n_finishers(head, 3))
+
+    # Linked List: Daisy -> Mario -> Toad -> Yoshi
+    print(top_n_finishers(head, 5))
+    print()
+    print("Problem 7: Remove Racer")
+    head = Node_v2("Daisy", Node_v2("Mario", Node_v2("Toad", Node_v2("Mario"))))
+    # Linked List: Daisy -> Mario -> Toad -> Mario
+    print_linked_list(remove_racer(head, "Mario"))
+
+    # Linked List: Daisy -> Mario -> Toad
+    print_linked_list(remove_racer(head, "Yoshi"))
+    print()
+    print("Problem 8: Array to Linked List")
+    mario = Player_array_to_linkedlist("Mario", "Mushmellow")
+    luigi = Player_array_to_linkedlist("Luigi", "Standard LG")
+    peach = Player_array_to_linkedlist("Peach", "Bumble V")
+
+    print_linked_list_char(arr_to_ll([mario, luigi, peach]))
+    print_linked_list_char(arr_to_ll([peach]))
+    print()
+    print("Problem 9: Convert Singly Linked List to Doubly Linked List")
+    koopa_troopa = Node_doubly("Koopa Troopa")
+    toadette = Node_doubly("Toadette")
+    waluigi = Node_doubly("Waluigi")
+    koopa_troopa.next = toadette
+    toadette.next = waluigi
+    print_linked_list(koopa_troopa)
+    print_linked_list_backwards(waluigi)
     print()
