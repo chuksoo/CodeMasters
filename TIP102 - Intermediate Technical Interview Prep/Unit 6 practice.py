ce8f14263c4a570fbeb41e05a6eb9939bd8fec8b
chuksoo <mexx4me@yahoo.com>
2025-03-25 19:25:58 -0800

    Started Unit 6 - Standard Problem Version 2 - Problem 2

--- TIP102 - Intermediate Technical Interview Prep/Unit 6 practice.py
+++ TIP102 - Intermediate Technical Interview Prep/Unit 6 practice.py
@@ -1,9 +1,17 @@
+###################################################
+# Session 1: Linked Lists II
+###################################################
+# Standard Problem Set Version 1
+# -------------------------------------------------
+'''Problem 1: Building a Playlist
+The assignment statement to the top_hits_2010s variable below creates the linked list Uptown Funk -> Party Rock Anthem -> Bad Romance. 
+Break apart the assignment statement into multiple lines with one call to the Node constructor per line to recreate the list.'''
+
 from collections import defaultdict
 
 class SongNode:
-    def __init__(self, song, artist, next=None):
+    def __init__(self, song, next=None):
         self.song = song
-        self.artist = artist
         self.next = next
 
 # For testing
@@ -13,36 +21,44 @@ def print_linked_list(node):
         print(current.song, end=" -> " if current.next else "")
         current = current.next
     print()
-        
-# node_three = SongNode("Bad Romance")
-# node_two = SongNode("Party Rock Anthem", node_three)
-# node_one = SongNode("Uptown Funk", node_two)
 
-#top_hits_2010s = node_one
+'''Problem 2: Top Artists
+Given the head of a linked list playlist, return a dictionary that maps each artist in the list to its frequency.
 
-# print_linked_list(top_hits_2010s)
+Evaluate the time complexity of your solution. Define your variables and provide a rationale for why you believe your solution has the stated time and space complexity.'''
+class SongNode_artist:
+    def __init__(self, song, artist, next=None):
+        self.song = song
+        self.artist = artist
+        self.next = next
 
 def get_artist_frequency(playlist):
-    # create dict
+    # create dict using default dict
     artist_count = defaultdict(int)
     current = playlist
-    # loop threw playlist 
+
     while current:
-        #if current.artist in artist_count:
+        # get count of current.artist in playlist:
         artist_count[current.artist] += 1
         current = current.next
-    
     return artist_count
-    # checking if current is in dict 
-    # if yes add one 
-    # else create new key in dict 
 
-playlist = SongNode("Saturn", "SZA", 
-                SongNode("Who", "Jimin", 
-                        SongNode("Espresso", "Sabrina Carpenter", 
-                                SongNode("Snooze", "SZA"))))
+'''Problem 3: Glitching Out
+The following code attempts to remove the first node with a given song from a singly linked list with head playlist_head but it contains a bug!
 
-# print(get_artist_frequency(playlist))
+Step 1: Copy this code into Replit.
+
+Step 2: Create your own test cases to run the code against, and use print statements and the stack trace to identify and fix the bug so that the function correctly removes a node by value from the list.
+
+Step 3: Evaluate the time and space complexity of the fixed solution. Define your variables and provide a rationale for why you believe the solution has the stated time and space complexity.'''
+
+# For testing
+def print_linked_list_tuple(node):
+    current = node
+    while current:
+        print((current.song, current.artist), end=" -> " if current.next else "")
+        current = current.next
+    print()
 
 # Function with a bug!
 def remove_song(playlist_head, song):
@@ -60,13 +76,13 @@ def remove_song(playlist_head, song):
 
     return playlist_head
 
-playlist = SongNode("SOS", "ABBA", 
-                SongNode("Simple Twist of Fate", "Bob Dylan",
-                    SongNode("Dreams", "Fleetwood Mac",
-                        SongNode("Lovely Day", "Bill Withers"))))
+'''Problem 4: On Repeat
+A variation of the two-pointer technique introduced in previous units is to have a slow and a fast pointer that increment at different rates.
 
-# print_linked_list(remove_song(playlist, "Dreams"))
+We would like to check whether our playlist loops or not. Given the head of a linked list playlist_head, return True if the playlist has a cycle in it and False otherwise. 
+A linked list has a cycle if at some point in the list, the node’s next pointer points back to a previous node in the list.
 
+Evaluate the time and space complexity of your solution. Define your variables and provide a rationale for why you believe your solution has the stated time and space complexity.'''
 def on_repeat(playlist_head):
     slow = fast = playlist_head
     while fast and fast.next:
@@ -76,42 +92,29 @@ def on_repeat(playlist_head):
             return True
     return False
 
-song1 = SongNode("GO!", "Common")
-song2 = SongNode("N95", "Kendrick Lamar")
-song3 = SongNode("WIN", "Jay Rock")
-song4 = SongNode("ATM", "J. Cole")
-song1.next = song2
-song2.next = song3
-song3.next = song4
-# song4.next = song2
+'''Problem 5: Looped
+Given the head of a linked list playlist_head that may contain a cycle, use the fast and slow pointer method to return the length of the cycle. If the list does not contain a cycle, return 0.
 
-# print(on_repeat(song1))
+Evaluate the time and space complexity of your solution. Define your variables and provide a rationale for why you believe your solution has the stated time and space complexity.'''
+def loop_length(playlist_head):
+    count = 0
+    slow = fast = playlist_head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        count += 1
+        if slow == fast:
+            return count 
+    return 0
 
-# def loop_length(playlist_head):
-#     count = 0
-#     slow = fast = playlist_head
-#     while fast and fast.next:
-#         slow = slow.next
-#         fast = fast.next.next
-#         count += 1
-#         if slow == fast:
-#             return count 
-#     return 0
+'''Problem 6: Volume Control
+You are working as an engineer normalizing volume levels on songs. Given the head of a singly linked list with integer values song_audio representing volume levels at different points in a song, 
+return the number of critical points. A critical point is a local minima or maxima.
 
-
-# song0 = SongNode("ioaiai", "ahufioahuf")
-# song1 = SongNode("Wein", "AL SHAMI")
-# song2 = SongNode("Si Ai", "Tayna")
-# song3 = SongNode("Qalbi", "Yasser Abd Alwahab")
-# song4 = SongNode("La", "DYSTINCT")
-# song0.next = song1
-# song1.next = song2
-# song2.next = song3
-# song3.next = song4
-# song4.next = song2
-
-# print(loop_length(song0))
-
+The head and tail nodes are not considered critical points.
+A node is a local minima if both the next and previous elements are greater than the current element
+A node is a local maxima if both the next and previous elements are less than the current element
+Evaluate the time and space complexity of your solution. Define your variables and provide a rationale for why you believe your solution has the stated time and space complexity.'''
 class Node:
     def __init__(self, value, next=None):
         self.value = value
@@ -131,7 +134,77 @@ def count_critical_points(song_audio):
         current = current.next
     return counter
 
+# -------------------------------------------------
+# Standard Problem Set Version 2
+# -------------------------------------------------
+'''Problem 1: Why is it Always You Three
+In a single assignment statement, create the linked list Harry -> Ron -> Hermione.'''
+# For testing
+def print_linked_list_value(head):
+    current = head
+    while current:
+        print(current.value, end=" -> " if current.next else "\n")
+        current = current.next
 
-song_audio = Node(5, Node(3, Node(1, Node(2, Node(5, Node(1, Node(2)))))))
+head = Node("Harry", Node("Ron", Node("Hermione")))
 
-print(count_critical_points(song_audio))
+
+if __name__ == "__main__":
+    print("-------- # Session 1: Linked Lists II -------- ")
+    print("------ # Standard Problem Set Version 1 ------ ")
+    print("Problem 1: Building a Playlist")
+    node_three = SongNode("Bad Romance")
+    node_two = SongNode("Party Rock Anthem", node_three)
+    node_one = SongNode("Uptown Funk", node_two)
+    top_hits_2010s = node_one
+    print_linked_list(top_hits_2010s)
+    print()
+    print("Problem 2: Top Artists")
+    playlist = SongNode_artist("Saturn", "SZA", 
+                    SongNode_artist("Who", "Jimin", 
+                            SongNode_artist("Espresso", "Sabrina Carpenter", 
+                                    SongNode_artist("Snooze", "SZA"))))
+    print(get_artist_frequency(playlist))
+    print()
+    print("Problem 3: Glitching Out")
+    playlist = SongNode_artist("SOS", "ABBA", 
+                    SongNode_artist("Simple Twist of Fate", "Bob Dylan",
+                        SongNode_artist("Dreams", "Fleetwood Mac",
+                            SongNode_artist("Lovely Day", "Bill Withers"))))
+    print_linked_list_tuple(remove_song(playlist, "Dreams"))
+    print()
+    print("Problem 4: On Repeat")
+    song1 = SongNode("GO!", "Common")
+    song2 = SongNode("N95", "Kendrick Lamar")
+    song3 = SongNode("WIN", "Jay Rock")
+    song4 = SongNode("ATM", "J. Cole")
+    song1.next = song2
+    song2.next = song3
+    song3.next = song4
+    song4.next = song2
+    print(on_repeat(song1))
+    print()
+    print("Problem 5: Looped")
+    song0 = SongNode("ioaiai", "ahufioahuf")
+    song1 = SongNode("Wein", "AL SHAMI")
+    song2 = SongNode("Si Ai", "Tayna")
+    song3 = SongNode("Qalbi", "Yasser Abd Alwahab")
+    song4 = SongNode("La", "DYSTINCT")
+    song0.next = song1
+    song1.next = song2
+    song2.next = song3
+    song3.next = song4
+    song4.next = song2
+    print(loop_length(song0))
+    print()
+    print("Problem 6: Volume Control")
+    song_audio = Node(5, Node(3, Node(1, Node(2, Node(5, Node(1, Node(2)))))))
+    print(count_critical_points(song_audio))
+    print()
+    print()
+    print("------ # Standard Problem Set Version 2 ------ ")
+    print("Problem 1: Why is it Always You Three")
+    print_linked_list_value(head)
+    print()
+
+    
